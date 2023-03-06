a9c06a7bb7e2e83842fee6ddc7c18a50882aafb8
Ilia Kamyab <kamyabfa@ualberta.ca>
2023-03-06 13:36:56 -0700

    Added methods for modelling the curve with SIR and gaussian models

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ import numpy as np
 import SubpopulationsLib.DataProcessing as dp
 import sklearn.metrics as metrics
 import scipy as sp
+from SubpopulationsLib.Subpopulations import find_theta_sa
+from SubpopulationsLib.Subpopulations import mixture_exponentials, mixture_SIR
 
 
 
@@ -34,19 +36,67 @@ def plot_data(data, country):
     return plt
 #method which returns the peaks of the data
 def get_peaks(data):
-    smoothed_data =  sp.signal.savgol_filter(data, len(data)//2,4)
+    smoothed_data =  sp.signal.savgol_filter(data, len(data)//2,3)
     peaks = sp.signal.find_peaks(smoothed_data)[0]
     return peaks, len(peaks)
 
+def fit_sir_model(data):
+    num_mixtures = get_peaks(data)[1]
+    bound_S = (0,1E8)
+    bound_beta = (0,1)
+    bound_gamma = (0,1)
+    bound_coef = (0,1000)
+    bound_k = (0,50)
+    bound_list_SIR = [bound_S, bound_beta, bound_gamma, bound_coef, bound_k]
+
+    bounds_SIR = list()
+
+    for element in bound_list_SIR:
+        for i in range(num_mixtures):
+            bounds_SIR.append(element)
+    bias = np.min(data)
+    norm_I = data - bias    
+    params_SIR = find_theta_sa(bounds_SIR, norm_I, mixture_SIR)
+    T = len(norm_I)
+    y_hat_SIR = mixture_SIR(params_SIR, T) + bias
+    
+    return y_hat_SIR
+
+def fit_gaussian_model(data):
+    num_mixtures = get_peaks(data)[1]
+    bounds_mu = (0,50)
+    bounds_sigma = (1,6)
+    bounds_coef = (0,300000)
+
+    bound_list_Gaussian = [bounds_mu, bounds_sigma, bounds_coef]
+
+    bounds_Gaussian = list()
+
+    for element in bound_list_Gaussian:
+        for i in range(num_mixtures):
+            bounds_Gaussian.append(element)
+    bias = np.min(data)
+    norm_I = data - bias
+
+    params_gaussian = find_theta_sa(bounds_Gaussian, norm_I, mixture_exponentials)
+    T = len(norm_I)
+    y_hat_gaussian = mixture_exponentials(params_gaussian, T) + bias
+    return y_hat_gaussian
+
+
+
 def main():
     path = "./Data/"
-    start_date = '11/30/20'
-    end_date = '4/30/21'
+    start_date = '7/30/20'
+    end_date = '7/30/21'
     country = 'Canada'
     data = read_data(path, start_date, end_date, country)
     plot = plot_data(data, country)
     peaks, num_peaks = get_peaks(data)
-
+    sir = fit_sir_model(data)
+    gaussian = fit_gaussian_model(data)
+    plot.plot(gaussian, color = 'red')
+    plot.plot(sir, color = 'green')
     plot.plot(peaks, data[peaks], 'bo')
     plot.show()
 
