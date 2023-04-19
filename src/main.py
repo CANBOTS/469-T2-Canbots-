cbe79e4ca737c82b058a7263fc51f7ecfa77673e
Ilia Kamyab <kamyabfa@ualberta.ca>
2023-04-18 22:57:59 -0600

    roberto creds

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ import datetime
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score, KFold
 
 from sklearn.model_selection import train_test_split
 
@@ -129,7 +130,7 @@ def plot_data(data, country,label):
 
 #method which returns the peaks and number of peaks for the data
 def get_peaks(data):
-    smoothed_data =  sp.signal.savgol_filter(data, len(data)//2,7)
+    smoothed_data =  sp.signal.savgol_filter(data, len(data)//2,5)
     peaks = sp.signal.find_peaks(smoothed_data)[0]
 
     if len(peaks) == 0:
@@ -190,12 +191,12 @@ def gaussian_params(data):
     return params_gaussian
 
 #forecasts the gaussian for a given number of weeks and returns the entire data
-def forecast_gaussian( data, params, steps, start_date, country,model):
+def forecast_gaussian( data, params, steps, start_date,end_date, country,model):
     bias = np.min(data)
     norm_I = data - bias
     T = len(norm_I)
     #print(params)
-    if tail_peak(data):
+    if tail_peak(data) or holiday_peak(end_date, country,steps):
         next_params = get_next_params(start_date, params, country,model)
 
         if next_params:
@@ -230,17 +231,39 @@ def plot_gaussians(params,data):
         #print(new_params)
         plt.plot(fit_gaussian_model(data, new_params), label = "Gaussian " + str(i+1))
 
-def is_holdiday(date, country):
+def get_holidays(country):
+    #make the first letter of the country lowercase
+    country = country[0].lower() + country[1:]
+    #get the holidays for the country
+    
+    df = pd.read_csv('./Data/holiday_calendar.csv')
+    df = df[df['Country'] == country]
+    holidays = df['Date'].tolist()
+    #delete the year from holidays
+    for i in range(len(holidays)):
+        holidays[i] = holidays[i][-5:]
+    #convert the holidays to datetime objects
+    
+
+    return holidays
+
+
+def holiday_peak(date, country,weeks):
     #get the holidays for the country
     holidays = get_holidays(country)
-    date = date[0:-2]+"20"+date[-2:] #converts date to format that datetime can read
-    date = datetime.datetime.strptime(date, "%m/%d/%Y")
-    #check if there is a holday within two weeks after the date
-    for i in range(1,15):
-        if date.strftime("%m/%d") in holidays:
+    #delete the year from the date
+    date = date[:-3]
+    date = datetime.datetime.strptime(date, "%m/%d")
+    date = date + datetime.timedelta(days=(weeks*7)-12)
+    #check if there is a holday within two weeks before the date
+    for i in range(14):
+        new_date = date - datetime.timedelta(days=i)
+        #convert dateime object to string
+        new_date = new_date.strftime("%m-%d")
+        if new_date in holidays:
             return True
-        date += datetime.timedelta(days=1)
     return False
+    
 
 #function to determine whether the end of the data set is a peak
 def tail_peak(data):
@@ -255,7 +278,7 @@ def tail_peak(data):
 
 #function to train a model to get the parameters for the next gaussian model based on the paramters of the previous gaussian curve
 def get_next_params(start_date, params, country,model):
-
+    print("bloop")
     if len(params) ==6:
         means = [params[0],params[1]]
         i = means.index(max(means))
@@ -267,6 +290,7 @@ def get_next_params(start_date, params, country,model):
     
     #Just so it doesnt fit a gaussian too far into the future, thends to mess with predictions 
     if 45<params[0]<51:
+        print("nooooo")
         return False
     mu = params[0] + 16 #assume that the next peak will be 18 weeks after the previous peak
     
@@ -276,20 +300,25 @@ def get_next_params(start_date, params, country,model):
     '''
     Use below code to use the regression model to get the next coefficient
     '''
+    if model:
     #convert start_date to a datetime object
-    # start_date = date_to_datetime(start_date)
-    # mu_date = start_date + datetime.timedelta(days=mu*7) #convert the number of weeks to a date
-    # modl = model
-    # feautures = get_feautures(mu_date, country)
-    # coef = modl.predict(feautures)
+        start_date = date_to_datetime(start_date)
+        mu_date = start_date + datetime.timedelta(days=mu*7) #convert the number of weeks to a date
+        modl = model
+        feautures = get_feautures(mu_date, country)
+        coef = modl.predict(feautures)[0]
+        print("reg")
+    else:
+        if len(params)>0:
+            coef = params[2]*1.1
+            print("nope")
+    print(mu,sigma,coef)
 
+    return mu,sigma,coef
     '''
     Use below code to use the previous coefficient to get the next coefficient
     '''
-    if len(params)>0:
-        coef = params[2]
-
-    return mu,sigma,coef
+    
 
 
 #Function to get the regression feuatures for a given date and country
@@ -369,6 +398,11 @@ def linear_regression():
     #fits the input data to a fifth degree polynomial
     poly = PolynomialFeatures(degree =5)
     x = poly.fit_transform(x)
+
+    #shuffle the x and y data
+    p = np.random.permutation(len(x))
+    x = x[p]
+    y = y[p]
     
     #splits the data into training and testing data
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
@@ -377,7 +411,17 @@ def linear_regression():
     model = LinearRegression().fit(x_train, y_train)
     print(model.score(x_test,y_test))
     model = LinearRegression().fit(x, y)
-    print(model.score(x,y))
+    # k = KFold(n_splits=10, shuffle=True)
+    # cv = cross_val_score(model, x, y, cv=k,scoring = "r2")
+
+    # cv = list(cv)
+    # cv.sort(reverse=True)
+    # cv.pop()
+
+    
+    #print(model.score(x,y))
+
+
     return model
 
 
@@ -397,7 +441,7 @@ def main():
     start_date = '7/30/20'
     end_date = '4/20/21'
     end_date2 = '5/20/21'
-    countries = ["Canada", "Australia", "Sweden", "United Kingdom","China","South Korea","Japan","Germany","Singapore"]
+    countries = ["Brazil","Colombia","Canada", "Australia", "Sweden", "United Kingdom","China","Japan","Germany","South Korea"]
     #country = "Mexico"
     
     
@@ -407,34 +451,54 @@ def main():
 
     #plt.plot(policies[0])
    # plt.plot(policies[1])
-    for_me = {}
-    model = 3
+    for_me1 = {}
+    for_me2 = {}
+    for_me3 = {}
+    for_me4 = {}
+    model = linear_regression()
     heights = {}
     errors ={}
     for country in countries:
         data = read_data(path, start_date, end_date, country)
 
         data2 = read_data(path, start_date, end_date2, country)
-        mapes = []
+        mapes1 = []
+        mapes2 = []
+        mapes3 = []
+        mapes4 = []
         for i in range(30):
             print(country,i)
             g_params = gaussian_params(data)
-            gaussian = forecast_gaussian(data, g_params,4,start_date, country, model)
-            mape = MAPE(data2[-4:], gaussian[-4:])[0]
-            mapes.append(mape)
-        interval = sp.stats.t.interval(confidence=0.95,
-              df=len(mapes)-1,
-              loc=np.mean(mapes), 
-              scale=sp.stats.sem(mapes))
-        heights[country] = interval[1]-interval[0]
-        errors[country] = np.mean(mapes)
-        for_me[country] = mapes
-    plt.bar(errors.keys(), errors.values(), yerr = heights.values(), color='g')
-    plt.title("MAPE assuming previous magnitude")
-    plt.ylabel("MAPE")
-    plt.xlabel("Country")
-    plt.ylim((0,100))
-    print(for_me)
+            gaussian1 = forecast_gaussian(data, g_params,1,start_date, country, model)
+            gaussian2 = forecast_gaussian(data, g_params,2,start_date, country, model)
+            gaussian3 = forecast_gaussian(data, g_params,3,start_date, country, model)
+            gaussian4 = forecast_gaussian(data, g_params,4,start_date, country, model)
+
+            mape1 = MAPE(data2[-4:-3], gaussian1[-1:])[0]
+            mape2 = MAPE(data2[-4:-2], gaussian2[-2:])[0]
+            mape3 = MAPE(data2[-4:-1], gaussian3[-3:])[0]
+            mape4 = MAPE(data2[-4:], gaussian4[-4:])[0]
+
+            mapes1.append(mape1)
+            mapes2.append(mape2)
+            mapes3.append(mape3)
+            mapes4.append(mape4)
+        
+        #heights[country] = interval[1]-interval[0]
+        #errors[country] = np.mean(mapes)
+        for_me1[country] = mapes1
+        for_me2[country] = mapes2
+        for_me3[country] = mapes3
+        for_me4[country] = mapes4
+    # plt.bar(errors.keys(), errors.values(), yerr = heights.values(), color='g')
+    # plt.title("MAPE assuming previous magnitude")
+    # plt.ylabel("MAPE")
+    # plt.xlabel("Country")
+    # plt.ylim((0,100))
+    print(for_me1)
+    print(for_me2)
+    print(for_me3)
+    print(for_me4)
     #plot = plot_data(data2, country,"data")
     # plt.plot(data, color = 'blue')
     #plt.plot(data2, color = 'green')
@@ -448,7 +512,7 @@ def main():
 
     #plt.plot(gaussian, color = 'red')
     # plt.legend()
-    plt.show()
+    #plt.show()
     
     #print("Gaussian MAPE: ", MAPE(data2, gaussian))
     # #print("SIR MSE: ", mse(data, sir))    
@@ -466,9 +530,9 @@ def main2():
     # # end_date = '2021-08-05'
     # country = 'Italy'
     start_date = '7/30/20'
-    end_date = '4/20/21'
-    end_date2 = '5/20/21'
-    country = "Singapore"
+    end_date = '5/10/21'
+    end_date2 = '6/10/21'
+    country = "China"
     
     
 
@@ -477,48 +541,87 @@ def main2():
 
     #plt.plot(policies[0])
    # plt.plot(policies[1])
-    model = linear_regression()
+    model1 = linear_regression()
+    model2 = False
     
     data = read_data(path, start_date, end_date, country)
 
     data2 = read_data(path, start_date, end_date2, country)
        
     g_params = gaussian_params(data)
-    gaussian = forecast_gaussian(data, g_params,4,start_date, country, model)
-
-    plt.plot(data, color = 'blue')
-    plt.plot(data2, color = 'green')
+    print(g_params)
+    # gaussian1 = forecast_gaussian(data, g_params,1,start_date, country, model)
+    # gaussian2 = forecast_gaussian(data, g_params,2,start_date, country, model)
+    # gaussian3 = forecast_gaussian(data, g_params,3,start_date, country, model)
+    gaussian1 = forecast_gaussian(data, g_params,4,start_date,end_date, country, model1)
+    gaussian2 = forecast_gaussian(data, g_params,4,start_date,end_date, country, model2)
+    #plt.plot(data, color = 'blue')
+    plt.plot(data2, color = 'green',label = "Actual")
     peaks, num_peaks = get_peaks(data)
     print(num_peaks)
     #sir = fit_sir_model(data)
+
     
     
     
     #gaussian = fit_gaussian_model(data, g_params)
-
-    plt.plot(gaussian, color = 'red')
+    plt.plot(gaussian2, color = 'blue', label = "Assumption")
+    plt.plot(gaussian1, color = 'red', label = "Linear Regression")
+    
+    # plt.plot(gaussian3, color = 'orange')
+    
+    # plt.plot(gaussian1, color = 'purple')
+    
     # plt.legend()
+    # print("Gaussian MAPE: ", MAPE(data2[-4:], gaussian[-4:]))
+    # print(gaussian[-1:], data2[-4:-3])
+    # mape1 = MAPE(data2[-4:-3], gaussian1[-1:])[0]
+    # mape2 = MAPE(data2[-4:-2], gaussian2[-2:])[0]
+    # mape3 = MAPE(data2[-4:-1], gaussian3[-3:])[0]
+    # mape4 = MAPE(data2[-4:], gaussian4[-4:])[0]
+    plt.xlabel("Weeks")
+    plt.ylabel("Infections")
+    #print(mape1, mape2, mape3, mape4)
+    plt.legend()
     plt.show()
     
-    print("Gaussian MAPE: ", MAPE(data2, gaussian))
+    
     # #print("SIR MSE: ", mse(data, sir))    
  
     # # # print("SIR MAPE: ", MAPE(data, sir))    
     # #plot.plot(sir, color = 'green')
     #plot.plot(peaks, data[peaks], 'bo')
+
+def peaks():
+    path = "./Data/"
+    start_date = '7/30/20'
+    end_date = '4/20/21'
+    end_date2 = '5/20/21'
+    countries = ["Brazil","Colombia","Canada", "Australia", "Sweden", "United Kingdom","China","Japan","Germany","South Korea"]
+
+    for country in countries:
+        data = read_data(path, start_date, end_date, country)
+        peaks, num_peaks = get_peaks(data)
+        if tail_peak(data) or holiday_peak(end_date, country,4):
+            num_peaks+=1
+        print(country, num_peaks)
+
+
 def graphics():
     path = "./Data/"
    
     start_date = '7/30/20'
-    end_date = '4/15/21'
-    country = "Japan"
+    end_date = '4/23/21'
+    country = "Singapore"
     data = read_data(path, start_date, end_date, country)
-    smoothed_data =  sp.signal.savgol_filter(data, len(data)//2,5)
-    plt.plot(data, color = 'blue',label = "Unsmoothed Data")
+    smoothed_data =  sp.signal.savgol_filter(data, len(data)//2,7)
+    plt.plot(data, color = 'blue',label = "Input Data")
     plt.plot(smoothed_data, color = 'red', label = "Smoothed Data")
     peaks, num_peaks = get_peaks(data)
 
-    plt.plot(peaks, data[peaks], 'bo', color = "green")
+    plt.plot(peaks, data[peaks], 'bo', color = "green",label = "Peaks")
+    plt.xlabel("Weeks from starting point")
+    plt.ylabel("Number of Infections")
 
     plt.legend()
     plt.show()
@@ -528,12 +631,10 @@ def graphics():
 
 #graphics()
 # print("regression")
-linear_regression()
+#linear_regression()
 #main()
-#print(population("US"))
-# print("neural")
-# neural_net()
-#print(get_csv_data("Canada", "2020-02-28", "2021-08-05"))
-#print(get_feautures("2020-06-28", "Canada"))
+main2()
+#peaks()
+#print(holiday_peak("12/24/21", "Canada",2))
 
     
